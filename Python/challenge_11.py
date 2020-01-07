d5b52862f48dd564e45d62ed50e36873dba73ba2
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 20:00:13 +0530

    Challenge 11

---
+++ Python/challenge_11.py
@@ -0,0 +1,19 @@
+"""
+Challenge: Lists
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+if __name__ == '__main__':
+    # N = int(input())
+    n = int(input())
+    l = []
+    for _ in range(n):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd !="print":
+            cmd += "("+ ",".join(args) +")"
+            eval("l."+cmd)
+        else:
+            print(l)
