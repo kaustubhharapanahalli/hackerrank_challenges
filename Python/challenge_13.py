3757664282af21827f91c9dec79ab55b48e0f014
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 20:00:21 +0530

    Challenge 13

---
+++ Python/challenge_13.py
@@ -0,0 +1,13 @@
+"""
+Challenge: sWAP cASE
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
