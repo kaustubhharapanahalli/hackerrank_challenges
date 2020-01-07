ecc6f6d3a1ac3954ecf10927a355b9f925ea704d
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 19:59:28 +0530

    Challenge 3

---
+++ Python/challenge_3.py
@@ -0,0 +1,13 @@
+"""
+Challenge: Arithmetic Operators
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+if __name__ == "__main__":
+    number_1 = int(input().strip())
+    number_2 = int(input().strip())
+
+    print(number_1 + number_2)
+    print(number_1 - number_2)
+    print(number_1 * number_2)
