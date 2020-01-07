6c091dfc8db6307a94845b70dea7d18d034392cb
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 20:00:54 +0530

    Challenge 18

---
+++ Python/challenge_18.py
@@ -0,0 +1,13 @@
+"""
+Challenge: String Validators
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+if __name__ == '__main__':
+    str = input()
+    print(any(c.isalnum()  for c in str))
+    print(any(c.isalpha() for c in str))
+    print(any(c.isdigit() for c in str))
+    print(any(c.islower() for c in str))
+    print(any(c.isupper() for c in str))
