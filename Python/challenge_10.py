ca9e67f96ef147d5c0e3ad99f6a941cd4050b32f
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 20:00:07 +0530

    Challenge 10

---
+++ Python/challenge_10.py
@@ -0,0 +1,20 @@
+"""
+Challenge: Finding the percentage
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    x =student_marks[query_name]
+    avg = 0
+    for i in x:
+        avg += i
+    avg = avg / len(x)
+    print('%.2f' %avg)
