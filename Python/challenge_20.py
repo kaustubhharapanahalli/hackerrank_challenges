8090f2970b39a0698373d67163940d8c18eb9426
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 20:01:05 +0530

    Challenge 20

---
+++ Python/challenge_20.py
@@ -0,0 +1,15 @@
+"""
+Challenge: Text Wrap
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+import textwrap
+
+def wrap(string, max_width):
+    return '\n'.join(textwrap.TextWrapper(width=max_width).wrap(text=string))
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
