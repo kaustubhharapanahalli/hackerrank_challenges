f6441fed436cfdc58e38259777861fabc601adac
Kaustubh M Harapanahalli <kaustubhharapanahalli@gmail.com>
2020-01-07 19:59:35 +0530

    Challenge 4

---
+++ Python/challenge_4.py
@@ -0,0 +1,11 @@
+"""
+Challenge: Python Division
+
+Author:    Kaustubh M. Harapanahalli
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(round(a/b, 12))
